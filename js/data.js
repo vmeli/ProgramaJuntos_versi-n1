c8aad8be30bc51287b7ce460d3b52d21bc421f39
vceli <vickensi.delacruz@gmail.com>
2016-08-09 17:33:58 -0500

    diagrama de barras dinamica

---
+++ js/data.js
@@ -0,0 +1,10 @@
+var poblacion = [
+					{ Region: 'Amazonas', Provincia: 'Bagua', Cantidad: 4 },
+					{ Region: 'Amazonas', Provincia: 'Bongara', Cantidad: 41 },
+					{ Region: 'Amazonas', Provincia: 'Chachapoyas', Cantidad: 316 },
+					{ Region: 'Ancash', Provincia: 'Antonio Raimondi', Cantidad: 47 },
+					{ Region: 'Ancash', Provincia: 'Bolognesi', Cantidad: 66 },
+					{ Region: 'Ancash', Provincia: 'Carhuaz', Cantidad: 80 },
+					{ Region: 'Cajamarca', Provincia: 'Cutervo', Cantidad: 70 },
+					{ Region: 'Cajamarca', Provincia: 'Jaen', Cantidad: 90 }
+					];
---
+++ js/demo.js
@@ -0,0 +1,62 @@
+(function(){
+
+	// var poblacion = [
+	// 				{ Region: 'América', Pais: 'Costa Rica', Cantidad: 4 },
+	// 				{ Region: 'América', Pais: 'Argentina', Cantidad: 41 },
+	// 				{ Region: 'América', Pais: 'Estado Unidos', Cantidad: 316 },
+	// 				{ Region: 'Europa', Pais: 'España', Cantidad: 47 },
+	// 				{ Region: 'Europa', Pais: 'Francia', Cantidad: 66 },
+	// 				{ Region: 'Europa', Pais: 'Alemania', Cantidad: 80 }
+	// 				];
+
+	var pobRegion_Keys = Enumerable.From(poblacion)
+								   .OrderBy(function(x) { return x.Region; })
+								   .Select(function(x) { return x.Region; })
+								   .Distinct()
+								   .ToArray(); 
+
+	var pobRegion_Values = Enumerable.From(poblacion)
+							         .OrderBy(function(x) { return x.Region; })
+							         .GroupBy(function(k) { return k.Region },
+							         	      function(v) { return { 
+                                                                		Region: v.Region,
+                                                       					Poblacion: v.Cantidad
+							         	      						}
+							         	      			  },
+							         	      function(key, group){
+							         	      			return	{
+							         	      				Region: key,
+							         	      				Poblacion: group.Sum(function(region){
+							         	      					return region.Poblacion;
+							         	      				})
+							         	      			}
+							         	      }
+							         )
+							         .Select(function(x) { return [ x.Region, x.Poblacion] })
+							         .ToArray();
+
+	var barChart = c3.generate({
+		bindto: '#c-bar',
+		data: {
+			columns  : pobRegion_Values,
+			keys     : { value: pobRegion_Keys },
+			type     : 'bar',
+			labels   : true,
+			color    : function(color, d){
+				var region = (d.id) ? d.id : d;
+				return color;
+			}
+		},
+		axis: {
+			x: {
+				type: 'category',
+				label: 'Region',
+				categories: ' '
+			},
+			y: {
+				label: 'Beneficiarios'
+			}
+		}
+	});
+
+}());
