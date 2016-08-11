94fcc9d6108ddab6ad0d60d62d3ae1dd91b0123a
vceli <vickensi.delacruz@gmail.com>
2016-08-11 04:26:16 -0500

    incluyendo data

--- js/data.js
+++ js/data.js
@@ -1,10 +1,757 @@
 var poblacion = [
-					{ Region: 'Amazonas', Provincia: 'Bagua', Cantidad: 4 },
-					{ Region: 'Amazonas', Provincia: 'Bongara', Cantidad: 41 },
-					{ Region: 'Amazonas', Provincia: 'Chachapoyas', Cantidad: 316 },
-					{ Region: 'Ancash', Provincia: 'Antonio Raimondi', Cantidad: 47 },
-					{ Region: 'Ancash', Provincia: 'Bolognesi', Cantidad: 66 },
-					{ Region: 'Ancash', Provincia: 'Carhuaz', Cantidad: 80 },
-					{ Region: 'Cajamarca', Provincia: 'Cutervo', Cantidad: 70 },
-					{ Region: 'Cajamarca', Provincia: 'Jaen', Cantidad: 90 }
-					];
+				  {
+				    "Region": "AMAZONAS",
+				    "Provincia": "BAGUA",
+				    "Cantidad": 6357
+				  },
+				  {
+				    "Region": "AMAZONAS",
+				    "Provincia": "BONGARA",
+				    "Cantidad": 2179
+				  },
+				  {
+				    "Region": "AMAZONAS",
+				    "Provincia": "CHACHAPOYAS",
+				    "Cantidad": 1979
+				  },
+				  {
+				    "Region": "AMAZONAS",
+				    "Provincia": "CONDORCANQUI",
+				    "Cantidad": 6833
+				  },
+				  {
+				    "Region": "AMAZONAS",
+				    "Provincia": "LUYA",
+				    "Cantidad": 4907
+				  },
+				  {
+				    "Region": "AMAZONAS",
+				    "Provincia": "RODRIGUEZ DE MENDOZA",
+				    "Cantidad": 448
+				  },
+				  {
+				    "Region": "AMAZONAS",
+				    "Provincia": "UTCUBAMBA",
+				    "Cantidad": 9527
+				  },
+				  {
+				    "Region": "ANCASH",
+				    "Provincia": "AIJA",
+				    "Cantidad": 249
+				  },
+				  {
+				    "Region": "ANCASH",
+				    "Provincia": "ANTONIO RAYMONDI",
+				    "Cantidad": 1478
+				  },
+				  {
+				    "Region": "ANCASH",
+				    "Provincia": "ASUNCION",
+				    "Cantidad": 373
+				  },
+				  {
+				    "Region": "ANCASH",
+				    "Provincia": "BOLOGNESI",
+				    "Cantidad": 343
+				  },
+				  {
+				    "Region": "ANCASH",
+				    "Provincia": "CARHUAZ",
+				    "Cantidad": 2564
+				  },
+				  {
+				    "Region": "ANCASH",
+				    "Provincia": "CARLOS F.FITZCARRALD",
+				    "Cantidad": 2574
+				  },
+				  {
+				    "Region": "ANCASH",
+				    "Provincia": "CASMA",
+				    "Cantidad": 1033
+				  },
+				  {
+				    "Region": "ANCASH",
+				    "Provincia": "CORONGO",
+				    "Cantidad": 385
+				  },
+				  {
+				    "Region": "ANCASH",
+				    "Provincia": "HUARAZ",
+				    "Cantidad": 1413
+				  },
+				  {
+				    "Region": "ANCASH",
+				    "Provincia": "HUARI",
+				    "Cantidad": 6330
+				  },
+				  {
+				    "Region": "ANCASH",
+				    "Provincia": "HUARMEY",
+				    "Cantidad": 387
+				  },
+				  {
+				    "Region": "ANCASH",
+				    "Provincia": "HUAYLAS",
+				    "Cantidad": 4546
+				  },
+				  {
+				    "Region": "ANCASH",
+				    "Provincia": "MARISCAL LUZURIAGA",
+				    "Cantidad": 2567
+				  },
+				  {
+				    "Region": "ANCASH",
+				    "Provincia": "OCROS",
+				    "Cantidad": 203
+				  },
+				  {
+				    "Region": "ANCASH",
+				    "Provincia": "PALLASCA",
+				    "Cantidad": 2715
+				  },
+				  {
+				    "Region": "ANCASH",
+				    "Provincia": "POMABAMBA",
+				    "Cantidad": 3303
+				  },
+				  {
+				    "Region": "ANCASH",
+				    "Provincia": "RECUAY",
+				    "Cantidad": 1047
+				  },
+				  {
+				    "Region": "ANCASH",
+				    "Provincia": "SANTA",
+				    "Cantidad": 1849
+				  },
+				  {
+				    "Region": "ANCASH",
+				    "Provincia": "SIHUAS",
+				    "Cantidad": 3355
+				  },
+				  {
+				    "Region": "ANCASH",
+				    "Provincia": "YUNGAY",
+				    "Cantidad": 3470
+				  },
+				  {
+				    "Region": "APURIMAC",
+				    "Provincia": "ABANCAY",
+				    "Cantidad": 6241
+				  },
+				  {
+				    "Region": "APURIMAC",
+				    "Provincia": "ANDAHUAYLAS",
+				    "Cantidad": 15511
+				  },
+				  {
+				    "Region": "APURIMAC",
+				    "Provincia": "ANTABAMBA",
+				    "Cantidad": 1514
+				  },
+				  {
+				    "Region": "APURIMAC",
+				    "Provincia": "AYMARAES",
+				    "Cantidad": 2926
+				  },
+				  {
+				    "Region": "APURIMAC",
+				    "Provincia": "CHINCHEROS",
+				    "Cantidad": 5608
+				  },
+				  {
+				    "Region": "APURIMAC",
+				    "Provincia": "COTABAMBAS",
+				    "Cantidad": 6250
+				  },
+				  {
+				    "Region": "APURIMAC",
+				    "Provincia": "GRAU",
+				    "Cantidad": 3077
+				  },
+				  {
+				    "Region": "AYACUCHO",
+				    "Provincia": "CANGALLO",
+				    "Cantidad": 4213
+				  },
+				  {
+				    "Region": "AYACUCHO",
+				    "Provincia": "HUAMANGA",
+				    "Cantidad": 10071
+				  },
+				  {
+				    "Region": "AYACUCHO",
+				    "Provincia": "HUANCA SANCOS",
+				    "Cantidad": 1025
+				  },
+				  {
+				    "Region": "AYACUCHO",
+				    "Provincia": "HUANTA",
+				    "Cantidad": 8587
+				  },
+				  {
+				    "Region": "AYACUCHO",
+				    "Provincia": "LA MAR",
+				    "Cantidad": 9016
+				  },
+				  {
+				    "Region": "AYACUCHO",
+				    "Provincia": "LUCANAS",
+				    "Cantidad": 3631
+				  },
+				  {
+				    "Region": "AYACUCHO",
+				    "Provincia": "PARINACOCHAS",
+				    "Cantidad": 1375
+				  },
+				  {
+				    "Region": "AYACUCHO",
+				    "Provincia": "PAUCAR DEL SARA SARA",
+				    "Cantidad": 788
+				  },
+				  {
+				    "Region": "AYACUCHO",
+				    "Provincia": "SUCRE",
+				    "Cantidad": 1058
+				  },
+				  {
+				    "Region": "AYACUCHO",
+				    "Provincia": "VICTOR FAJARDO",
+				    "Cantidad": 2142
+				  },
+				  {
+				    "Region": "AYACUCHO",
+				    "Provincia": "VILCAS HUAMAN",
+				    "Cantidad": 2408
+				  },
+				  {
+				    "Region": "CAJAMARCA",
+				    "Provincia": "CAJABAMBA",
+				    "Cantidad": 6940
+				  },
+				  {
+				    "Region": "CAJAMARCA",
+				    "Provincia": "CAJAMARCA",
+				    "Cantidad": 10613
+				  },
+				  {
+				    "Region": "CAJAMARCA",
+				    "Provincia": "CELENDIN",
+				    "Cantidad": 9899
+				  },
+				  {
+				    "Region": "CAJAMARCA",
+				    "Provincia": "CHOTA",
+				    "Cantidad": 12614
+				  },
+				  {
+				    "Region": "CAJAMARCA",
+				    "Provincia": "CONTUMAZA",
+				    "Cantidad": 2133
+				  },
+				  {
+				    "Region": "CAJAMARCA",
+				    "Provincia": "CUTERVO",
+				    "Cantidad": 17941
+				  },
+				  {
+				    "Region": "CAJAMARCA",
+				    "Provincia": "HUALGAYOC",
+				    "Cantidad": 10916
+				  },
+				  {
+				    "Region": "CAJAMARCA",
+				    "Provincia": "JAEN",
+				    "Cantidad": 7895
+				  },
+				  {
+				    "Region": "CAJAMARCA",
+				    "Provincia": "SAN IGNACIO",
+				    "Cantidad": 13265
+				  },
+				  {
+				    "Region": "CAJAMARCA",
+				    "Provincia": "SAN MARCOS",
+				    "Cantidad": 5034
+				  },
+				  {
+				    "Region": "CAJAMARCA",
+				    "Provincia": "SAN MIGUEL",
+				    "Cantidad": 5744
+				  },
+				  {
+				    "Region": "CAJAMARCA",
+				    "Provincia": "SAN PABLO",
+				    "Cantidad": 2951
+				  },
+				  {
+				    "Region": "CAJAMARCA",
+				    "Provincia": "SANTA CRUZ",
+				    "Cantidad": 4216
+				  },
+				  {
+				    "Region": "CUSCO",
+				    "Provincia": "ACOMAYO",
+				    "Cantidad": 2614
+				  },
+				  {
+				    "Region": "CUSCO",
+				    "Provincia": "ANTA",
+				    "Cantidad": 4368
+				  },
+				  {
+				    "Region": "CUSCO",
+				    "Provincia": "CALCA",
+				    "Cantidad": 4754
+				  },
+				  {
+				    "Region": "CUSCO",
+				    "Provincia": "CANAS",
+				    "Cantidad": 3947
+				  },
+				  {
+				    "Region": "CUSCO",
+				    "Provincia": "CANCHIS",
+				    "Cantidad": 6828
+				  },
+				  {
+				    "Region": "CUSCO",
+				    "Provincia": "CHUMBIVILCAS",
+				    "Cantidad": 9247
+				  },
+				  {
+				    "Region": "CUSCO",
+				    "Provincia": "CUSCO",
+				    "Cantidad": 304
+				  },
+				  {
+				    "Region": "CUSCO",
+				    "Provincia": "ESPINAR",
+				    "Cantidad": 4470
+				  },
+				  {
+				    "Region": "CUSCO",
+				    "Provincia": "LA CONVENCION",
+				    "Cantidad": 6681
+				  },
+				  {
+				    "Region": "CUSCO",
+				    "Provincia": "PARURO",
+				    "Cantidad": 3486
+				  },
+				  {
+				    "Region": "CUSCO",
+				    "Provincia": "PAUCARTAMBO",
+				    "Cantidad": 5295
+				  },
+				  {
+				    "Region": "CUSCO",
+				    "Provincia": "QUISPICANCHI",
+				    "Cantidad": 8708
+				  },
+				  {
+				    "Region": "CUSCO",
+				    "Provincia": "URUBAMBA",
+				    "Cantidad": 1167
+				  },
+				  {
+				    "Region": "HUANCAVELICA",
+				    "Provincia": "ACOBAMBA",
+				    "Cantidad": 5616
+				  },
+				  {
+				    "Region": "HUANCAVELICA",
+				    "Provincia": "ANGARAES",
+				    "Cantidad": 5353
+				  },
+				  {
+				    "Region": "HUANCAVELICA",
+				    "Provincia": "CASTROVIRREYNA",
+				    "Cantidad": 1780
+				  },
+				  {
+				    "Region": "HUANCAVELICA",
+				    "Provincia": "CHURCAMPA",
+				    "Cantidad": 4195
+				  },
+				  {
+				    "Region": "HUANCAVELICA",
+				    "Provincia": "HUANCAVELICA",
+				    "Cantidad": 11269
+				  },
+				  {
+				    "Region": "HUANCAVELICA",
+				    "Provincia": "HUAYTARA",
+				    "Cantidad": 1896
+				  },
+				  {
+				    "Region": "HUANCAVELICA",
+				    "Provincia": "TAYACAJA",
+				    "Cantidad": 10410
+				  },
+				  {
+				    "Region": "HUANUCO",
+				    "Provincia": "AMBO",
+				    "Cantidad": 6024
+				  },
+				  {
+				    "Region": "HUANUCO",
+				    "Provincia": "DOS DE MAYO",
+				    "Cantidad": 4177
+				  },
+				  {
+				    "Region": "HUANUCO",
+				    "Provincia": "HUACAYBAMBA",
+				    "Cantidad": 2399
+				  },
+				  {
+				    "Region": "HUANUCO",
+				    "Provincia": "HUAMALIES",
+				    "Cantidad": 7373
+				  },
+				  {
+				    "Region": "HUANUCO",
+				    "Provincia": "HUANUCO",
+				    "Cantidad": 10686
+				  },
+				  {
+				    "Region": "HUANUCO",
+				    "Provincia": "LAURICOCHA",
+				    "Cantidad": 2668
+				  },
+				  {
+				    "Region": "HUANUCO",
+				    "Provincia": "LEONCIO PRADO",
+				    "Cantidad": 5613
+				  },
+				  {
+				    "Region": "HUANUCO",
+				    "Provincia": "MARAÑON",
+				    "Cantidad": 3601
+				  },
+				  {
+				    "Region": "HUANUCO",
+				    "Provincia": "PACHITEA",
+				    "Cantidad": 7344
+				  },
+				  {
+				    "Region": "HUANUCO",
+				    "Provincia": "PUERTO INCA",
+				    "Cantidad": 1877
+				  },
+				  {
+				    "Region": "HUANUCO",
+				    "Provincia": "YAROWILCA",
+				    "Cantidad": 3448
+				  },
+				  {
+				    "Region": "JUNIN",
+				    "Provincia": "CHUPACA",
+				    "Cantidad": 2530
+				  },
+				  {
+				    "Region": "JUNIN",
+				    "Provincia": "CONCEPCION",
+				    "Cantidad": 3471
+				  },
+				  {
+				    "Region": "JUNIN",
+				    "Provincia": "HUANCAYO",
+				    "Cantidad": 5368
+				  },
+				  {
+				    "Region": "JUNIN",
+				    "Provincia": "JAUJA",
+				    "Cantidad": 3482
+				  },
+				  {
+				    "Region": "JUNIN",
+				    "Provincia": "JUNIN",
+				    "Cantidad": 959
+				  },
+				  {
+				    "Region": "JUNIN",
+				    "Provincia": "SATIPO",
+				    "Cantidad": 10024
+				  },
+				  {
+				    "Region": "JUNIN",
+				    "Provincia": "TARMA",
+				    "Cantidad": 2158
+				  },
+				  {
+				    "Region": "JUNIN",
+				    "Provincia": "YAULI",
+				    "Cantidad": 410
+				  },
+				  {
+				    "Region": "LA LIBERTAD",
+				    "Provincia": "ASCOPE",
+				    "Cantidad": 1710
+				  },
+				  {
+				    "Region": "LA LIBERTAD",
+				    "Provincia": "BOLIVAR",
+				    "Cantidad": 1769
+				  },
+				  {
+				    "Region": "LA LIBERTAD",
+				    "Provincia": "CHEPEN",
+				    "Cantidad": 3942
+				  },
+				  {
+				    "Region": "LA LIBERTAD",
+				    "Provincia": "GRAN CHIMU",
+				    "Cantidad": 2503
+				  },
+				  {
+				    "Region": "LA LIBERTAD",
+				    "Provincia": "JULCAN",
+				    "Cantidad": 4101
+				  },
+				  {
+				    "Region": "LA LIBERTAD",
+				    "Provincia": "OTUZCO",
+				    "Cantidad": 8521
+				  },
+				  {
+				    "Region": "LA LIBERTAD",
+				    "Provincia": "PACASMAYO",
+				    "Cantidad": 1340
+				  },
+				  {
+				    "Region": "LA LIBERTAD",
+				    "Provincia": "PATAZ",
+				    "Cantidad": 8464
+				  },
+				  {
+				    "Region": "LA LIBERTAD",
+				    "Provincia": "SANCHEZ CARRION",
+				    "Cantidad": 18171
+				  },
+				  {
+				    "Region": "LA LIBERTAD",
+				    "Provincia": "SANTIAGO DE CHUCO",
+				    "Cantidad": 5906
+				  },
+				  {
+				    "Region": "LA LIBERTAD",
+				    "Provincia": "TRUJILLO",
+				    "Cantidad": 4984
+				  },
+				  {
+				    "Region": "LA LIBERTAD",
+				    "Provincia": "VIRU",
+				    "Cantidad": 4216
+				  },
+				  {
+				    "Region": "LORETO",
+				    "Provincia": "ALTO AMAZONAS",
+				    "Cantidad": 12132
+				  },
+				  {
+				    "Region": "LORETO",
+				    "Provincia": "DATEM DEL MARAÑON",
+				    "Cantidad": 6861
+				  },
+				  {
+				    "Region": "LORETO",
+				    "Provincia": "LORETO",
+				    "Cantidad": 6792
+				  },
+				  {
+				    "Region": "LORETO",
+				    "Provincia": "MARISCAL RAMON CASTILLA",
+				    "Cantidad": 4713
+				  },
+				  {
+				    "Region": "LORETO",
+				    "Provincia": "MAYNAS",
+				    "Cantidad": 13550
+				  },
+				  {
+				    "Region": "LORETO",
+				    "Provincia": "PUTUMAYO",
+				    "Cantidad": 674
+				  },
+				  {
+				    "Region": "LORETO",
+				    "Provincia": "REQUENA",
+				    "Cantidad": 7244
+				  },
+				  {
+				    "Region": "LORETO",
+				    "Provincia": "UCAYALI",
+				    "Cantidad": 5198
+				  },
+				  {
+				    "Region": "PASCO",
+				    "Provincia": "DANIEL ALCIDES CARRION",
+				    "Cantidad": 2610
+				  },
+				  {
+				    "Region": "PASCO",
+				    "Provincia": "OXAPAMPA",
+				    "Cantidad": 4734
+				  },
+				  {
+				    "Region": "PASCO",
+				    "Provincia": "PASCO",
+				    "Cantidad": 4204
+				  },
+				  {
+				    "Region": "PIURA",
+				    "Provincia": "AYABACA",
+				    "Cantidad": 15494
+				  },
+				  {
+				    "Region": "PIURA",
+				    "Provincia": "HUANCABAMBA",
+				    "Cantidad": 16306
+				  },
+				  {
+				    "Region": "PIURA",
+				    "Provincia": "MORROPON",
+				    "Cantidad": 16038
+				  },
+				  {
+				    "Region": "PIURA",
+				    "Provincia": "PAITA",
+				    "Cantidad": 1298
+				  },
+				  {
+				    "Region": "PIURA",
+				    "Provincia": "PIURA",
+				    "Cantidad": 26296
+				  },
+				  {
+				    "Region": "PIURA",
+				    "Provincia": "SECHURA",
+				    "Cantidad": 1466
+				  },
+				  {
+				    "Region": "PIURA",
+				    "Provincia": "SULLANA",
+				    "Cantidad": 2999
+				  },
+				  {
+				    "Region": "PIURA",
+				    "Provincia": "TALARA",
+				    "Cantidad": 843
+				  },
+				  {
+				    "Region": "PUNO",
+				    "Provincia": "AZANGARO",
+				    "Cantidad": 11202
+				  },
+				  {
+				    "Region": "PUNO",
+				    "Provincia": "CARABAYA",
+				    "Cantidad": 6604
+				  },
+				  {
+				    "Region": "PUNO",
+				    "Provincia": "CHUCUITO",
+				    "Cantidad": 5484
+				  },
+				  {
+				    "Region": "PUNO",
+				    "Provincia": "EL COLLAO",
+				    "Cantidad": 3211
+				  },
+				  {
+				    "Region": "PUNO",
+				    "Provincia": "HUANCANE",
+				    "Cantidad": 4410
+				  },
+				  {
+				    "Region": "PUNO",
+				    "Provincia": "LAMPA",
+				    "Cantidad": 2847
+				  },
+				  {
+				    "Region": "PUNO",
+				    "Provincia": "MELGAR",
+				    "Cantidad": 5958
+				  },
+				  {
+				    "Region": "PUNO",
+				    "Provincia": "MOHO",
+				    "Cantidad": 1738
+				  },
+				  {
+				    "Region": "PUNO",
+				    "Provincia": "PUNO",
+				    "Cantidad": 6725
+				  },
+				  {
+				    "Region": "PUNO",
+				    "Provincia": "SAN ANTONIO DE PUTINA",
+				    "Cantidad": 1847
+				  },
+				  {
+				    "Region": "PUNO",
+				    "Provincia": "SAN ROMAN",
+				    "Cantidad": 2461
+				  },
+				  {
+				    "Region": "PUNO",
+				    "Provincia": "SANDIA",
+				    "Cantidad": 3957
+				  },
+				  {
+				    "Region": "PUNO",
+				    "Provincia": "YUNGUYO",
+				    "Cantidad": 1531
+				  },
+				  {
+				    "Region": "SAN MARTIN",
+				    "Provincia": "BELLAVISTA",
+				    "Cantidad": 2136
+				  },
+				  {
+				    "Region": "SAN MARTIN",
+				    "Provincia": "EL DORADO",
+				    "Cantidad": 2495
+				  },
+				  {
+				    "Region": "SAN MARTIN",
+				    "Provincia": "HUALLAGA",
+				    "Cantidad": 1798
+				  },
+				  {
+				    "Region": "SAN MARTIN",
+				    "Provincia": "LAMAS",
+				    "Cantidad": 5720
+				  },
+				  {
+				    "Region": "SAN MARTIN",
+				    "Provincia": "MARISCAL CACERES",
+				    "Cantidad": 1511
+				  },
+				  {
+				    "Region": "SAN MARTIN",
+				    "Provincia": "MOYOBAMBA",
+				    "Cantidad": 2935
+				  },
+				  {
+				    "Region": "SAN MARTIN",
+				    "Provincia": "PICOTA",
+				    "Cantidad": 1110
+				  },
+				  {
+				    "Region": "SAN MARTIN",
+				    "Provincia": "RIOJA",
+				    "Cantidad": 6479
+				  },
+				  {
+				    "Region": "SAN MARTIN",
+				    "Provincia": "SAN MARTIN",
+				    "Cantidad": 2563
+				  },
+				  {
+				    "Region": "SAN MARTIN",
+				    "Provincia": "TOCACHE",
+				    "Cantidad": 73
+				  }
+				]
